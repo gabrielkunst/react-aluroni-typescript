926f6a937f99c1e1d15c2238e36d4dff64f8a0b5
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-04-29 11:42:05 -0300

    extensions name fixed

--- app/src/pages/item/Item.tsx
+++ app/src/pages/item/Item.tsx
@@ -6,34 +6,34 @@ import NotFound from "../notfound/NotFound";
 import DefaultPage from "@/components/defaultPage/DefaultPage";
 
 export default function Products() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const currentProduct = productsList.find((item) => item.id === Number(id));
-  if (!currentProduct) {
-    return <NotFound />;
-  }
-  return (
-    <DefaultPage>
-      <section className={style.itemContainer}>
-        <button className={style.button} onClick={() => navigate(-1)}>
-          {"< Voltar"}
-        </button>
-        <div className={style.item}>
-          <h1 className={style.item__title}>{currentProduct?.title}</h1>
-          <div className={style.item__image}>
-            <img src={currentProduct?.photo} alt={currentProduct?.title} />
-          </div>
-          <div className={style.item__content}>
-            <p>{currentProduct?.description}</p>
-            <div className={style.item__content__labels}>
-              <div>{currentProduct?.category.label}</div>
-              <div>{currentProduct?.size}g</div>
-              <div>Serve {currentProduct?.serving} pessoas</div>
-              <div>R$ {currentProduct?.price?.toFixed(2)}</div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </DefaultPage>
-  );
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const currentProduct = productsList.find((item) => item.id === Number(id));
+	if (!currentProduct) {
+		return <NotFound />;
+	}
+	return (
+		<DefaultPage>
+			<section className={style.itemContainer}>
+				<button className={style.button} onClick={() => navigate(-1)}>
+					{"< Voltar"}
+				</button>
+				<div className={style.item}>
+					<h1 className={style.item__title}>{currentProduct?.title}</h1>
+					<div className={style.item__image}>
+						<img src={currentProduct?.photo} alt={currentProduct?.title} />
+					</div>
+					<div className={style.item__content}>
+						<p>{currentProduct?.description}</p>
+						<div className={style.item__content__labels}>
+							<div>{currentProduct?.category.label}</div>
+							<div>{currentProduct?.size}g</div>
+							<div>Serve {currentProduct?.serving} pessoas</div>
+							<div>R$ {currentProduct?.price?.toFixed(2)}</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</DefaultPage>
+	);
 }
--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Menu from "./pages/menu/Menu.tsx";
-import DefaultPage from "./components/defaultPage/DefaultPage.tsx";
-import Home from "./pages/home/Home.tsx";
-import Navbar from "./components/navbar/Navbar.tsx";
-import About from "./pages/about/About.tsx";
-import NotFound from "./pages/notfound/NotFound.tsx";
+import Menu from "./pages/menu/Menu";
+import DefaultPage from "./components/defaultPage/DefaultPage";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import NotFound from "./pages/notfound/NotFound";
 import "./index.scss";
-import Item from "./pages/item/Item.tsx";
+import Item from "./pages/item/Item";
 
 export default function AppRouter() {
 	return (
